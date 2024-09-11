96f5cbc8d8359ac1f88c57c5089beacd37c82de8
xuaanhaor <eji0nkiz@gmail.com>
2024-09-12 00:04:01 +0700

    fix movement & finish game logic

--- snake.py
+++ snake.py
@@ -30,8 +30,8 @@ food = turtle.Turtle()
 colors = random.choice(['red', 'green', 'blue'])
 shapes = random.choice(['square', 'triangle', 'circle'])
 food.speed(0)
-food.shape(shapes)
-food.color(colors)
+food.shape('square')
+food.color('green')
 food.penup()
 food.goto(0, 100)
 
@@ -71,10 +71,10 @@ def move():
         head.sety(y - 20)
     if head.direction == 'left':
         x = head.xcor()
-        head.sety(x - 20)
+        head.setx(x - 20)
     if head.direction == 'right':
         x = head.xcor()
-        head.sety(x + 20)
+        head.setx(x + 20)
 
 # Keyboard bindings
 window.listen()
@@ -82,3 +82,65 @@ window.onkeypress(group, 'w')
 window.onkeypress(go_down, 's')
 window.onkeypress(go_left, 'a')
 window.onkeypress(go_right, 'd')
+
+segments = []
+
+while True:
+    window.update()
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = 'Stop'
+        colors = random.choice(['red', 'green', 'blue'])
+        shapes = random.choice(['square', 'triangle', 'circle'])
+        for segment in segments:
+            segment.goto(1000, 1000)
+        segments.clear()
+        score = 0
+        delay = 0.1
+        pen.clear()
+        pen.write("Score: {} High Score: {}".format(score, high_score), align='center', font=('Monospace', 24, 'normal'))
+    if head.distance(food) < 20:
+        x = random.randint(-270, 270)
+        y = random.randint(-270, 270)
+        food.goto(x, y)
+
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape('square')
+        new_segment.color('orange')
+        new_segment.penup()
+        segments.append(new_segment)
+        delay -= 0.001
+        score += 10
+        if score > high_score:
+            high_score = score
+        pen.clear()
+        pen.write("Score: {} High Score: {}".format(score, high_score), align='center', font=('Monospace', 24, 'normal'))
+    # Check for head collisions with body segments
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+    move()
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = 'stop'
+            colors = random.choice(['red', 'green', 'blue'])
+            shapes = random.choice(['square', 'triangle', 'circle'])
+            for segment in segments:
+                segment.goto(1000, 1000)
+            segment.clear()
+            score = 0
+            delay = 0.1
+            pen.clear()
+            pen.write("Score: {} High Score: {}".format(score, high_score), align='center', font=('Monospace', 24, 'normal'))
+    time.sleep(delay)
+window.mainloop()
